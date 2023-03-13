5db202a7d62233e2b2f1a07dae2528effbb7b979
gongcy <gongcy126@126.com>
2023-03-13 11:35:35 +0800

    refactor: optimize code

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ apt-get install python-pymysql python-serial -y
 
 python -m pip install --upgrade pip setuptools wheel
 pip install Adafruit-SSD1306
-pip install -r data/lib.txt
+pip install -r requirements.txt
