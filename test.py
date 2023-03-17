fdd83ec0db8bf4099612d38801ef95030a801d18
gongcy <gongcy126@126.com>
2023-03-18 00:36:49 +0800

    add test

--- test.py
+++ test.py
@@ -11,12 +11,14 @@ while True:
     # ser.write(bytes.fromhex('ff 01 78 00 00 00 00 00 87'))
     time.sleep(1)
     response = ser.read(9)
-
+    print(f"response: {response}")
     if response[0] == 0xFF and response[1] == 0x17:
         high_byte = response[2]
         low_byte = response[3]
         concentration = ((high_byte << 8) + low_byte) / 1000.0
         # ch2o_ppm = (high_byte << 8) | low_byte
         print("Formaldehyde concentration: %.3f ppm" % concentration)
+    else:
+        print("error")
 
     time.sleep(1)
