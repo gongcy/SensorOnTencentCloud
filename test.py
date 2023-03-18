3139f202ada19277a5cd476ec3e3865d32b846fd
gongcy <gongcy126@126.com>
2023-03-18 09:23:22 +0800

    optimize

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 _*-
+import sys
 import time
 
 import serial
@@ -14,17 +15,30 @@ DISABLE_AUTO_SUBMIT = b"\xFF\x01\x78\x41\x00\x00\x00\x00\x46"
 # 问答模式，读浓度
 QUERY_CONCENTRATION = b"\xFF\x01\x86\x00\x00\x00\x00\x00\x79"
 
-ser.write(ENABLE_AUTO_SUBMIT)
-while True:
-    # ser.write(bytes.fromhex('ff 01 78 00 00 00 00 00 87'))
-    time.sleep(1)
-    response = ser.read(9)
-    print(f"response: {response}")
-    if response[0] == 0xFF and response[1] == 0x17:
-        high_byte = response[4]
-        low_byte = response[5]
-        concentration = ((high_byte << 8) + low_byte) / 1000.0
-        # ch2o_ppm = (high_byte << 8) | low_byte
-        print("Formaldehyde concentration: %.3f ppm" % concentration)
-    else:
-        print("error")
+
+def get_concentration(passive=False):
+    model = ENABLE_AUTO_SUBMIT
+    if passive:
+        model = DISABLE_AUTO_SUBMIT
+    ser.write(model)
+    while True:
+        # ser.write(bytes.fromhex('ff 01 78 00 00 00 00 00 87'))
+        time.sleep(1)
+        response = ser.read(9)
+        print(f"response: {response}")
+        if response[0] == 0xFF and response[1] == 0x17:
+            high_byte = response[4]
+            low_byte = response[5]
+            concentration = ((high_byte << 8) + low_byte) / 1000.0
+            # ch2o_ppm = (high_byte << 8) | low_byte
+            print("Formaldehyde concentration: %.3f ppm" % concentration)
+        else:
+            print("error")
+
+
+if __name__ == '__main__':
+    passive = False
+    if len(sys.argv) > 1:
+        passive = True
+
+    get_concentration(passive)
