8d0a24d8ddbc34fa4a27dd7e2cc8b48e82ddfe4f
gongcy <gongcy126@126.com>
2023-03-22 17:12:21 +0800

    optimize code

--- lib/dht22.py
+++ lib/dht22.py
@@ -3,8 +3,9 @@ import Adafruit_DHT
 
 
 def get_dht_data():
+    pin = 4
     sensor = Adafruit_DHT.DHT22
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
         return temperature, humidity
     return -1, -1
