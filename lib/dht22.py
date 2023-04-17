4a79a13f79a9f8328d4a9ad00a56caa29c9b6792
gongcy <gongcy126@126.com>
2023-04-17 17:02:06 +0800

    refactor: update adafruit_dht package

--- lib/dht22.py
+++ lib/dht22.py
@@ -1,11 +1,22 @@
 # -*- coding:utf-8 _*-
-import Adafruit_DHT
+import adafruit_dht
+from board import D4
+
+# See: https://github.com/adafruit/Adafruit_CircuitPython_DHT
+
+dht_device = adafruit_dht.DHT22(D4)
 
 
 def get_dht_data():
-    pin = 4
-    sensor = Adafruit_DHT.DHT22
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    if humidity is not None and temperature is not None:
-        return temperature, humidity
-    return -1, -1
+    try:
+        temperature = dht_device.temperature
+        humidity = dht_device.humidity
+        if humidity is not None and temperature is not None:
+            return temperature, humidity
+        return -1, -1
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+    except Exception as error:
+        dht_device.exit()
+        raise error
