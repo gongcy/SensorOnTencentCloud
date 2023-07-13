6a22b9d76b691c5d532d22ade638ec313f1a4df2
gongcy <gongcy126@126.com>
2023-07-13 11:09:30 +0800

    fix: change dht package to old version

--- lib/dht22.py
+++ lib/dht22.py
@@ -1,22 +1,32 @@
-# -*- coding:utf-8 _*-
-import adafruit_dht
-from board import D4
-
-# See: https://github.com/adafruit/Adafruit_CircuitPython_DHT
-
-dht_device = adafruit_dht.DHT22(D4)
+import Adafruit_DHT
 
 
 def get_dht_data():
-    try:
-        temperature = dht_device.temperature
-        humidity = dht_device.humidity
-        if humidity is not None and temperature is not None:
-            return temperature, humidity
-        return -1, -1
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-    except Exception as error:
-        dht_device.exit()
-        raise error
+    pin = 4
+    sensor = Adafruit_DHT.DHT22
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+        return temperature, humidity
+    return -1, -1
+# # -*- coding:utf-8 _*-
+# import adafruit_dht
+# from board import D4
+#
+# # See: https://github.com/adafruit/Adafruit_CircuitPython_DHT
+#
+# dht_device = adafruit_dht.DHT22(D4)
+#
+#
+# def get_dht_data():
+#     try:
+#         temperature = dht_device.temperature
+#         humidity = dht_device.humidity
+#         if humidity is not None and temperature is not None:
+#             return temperature, humidity
+#         return -1, -1
+#     except RuntimeError as error:
+#         # Errors happen fairly often, DHT's are hard to read, just keep going
+#         print(error.args[0])
+#     except Exception as error:
+#         dht_device.exit()
+#         raise error
